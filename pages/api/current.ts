20b771899fa0357ad8b81140bc8686da12d5a372
Jakub Chmiel <jakubpchmiel@gmail.com>
2023-12-19 17:00:12 +0100

    Update current.ts

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -8,6 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const { currentUser } = await serverAuth(req, res);
+    const test = "123";
 
     return res.status(200).json(currentUser);
   } catch (error) {
